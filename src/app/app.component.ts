2ed1cd9770ce0de9581ba2c1f97e3fab237173a0
Hina Chauhan <35649848+hinachauhan09@users.noreply.github.com>
2019-03-15 17:03:16 +0530

    Small fix
    When date of month selected is less than date of start_date then it was not giving proper output. That fixed. Now working properly. Enjoy.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,25 +8,29 @@ import * as moment from "moment";
 })
 export class AppComponent {
   title = "Finding specific dates in given date range & count";
-  start_date = moment();
+  start_date = moment("2019-03-04");
   end_date = moment(this.start_date).add(3, "months");
   total_days = this.end_date.diff(this.start_date, "days");
-  selected_day = "31,2";
+  selected_day = "2,31";
   day_array = [];
   myday = this.start_date;
   count = 0;
 
   constructor() {
     this.selected_day.split(",").forEach(day => {
+      let offset = 0;
+      if (day < this.myday.get("date")) {
+        offset = 1;
+      }
       let days = [
         moment(this.myday)
-          .add(0, "months")
+          .add(0 + offset, "months")
           .set("date", day),
         moment(this.myday)
-          .add(1, "months")
+          .add(1 + offset, "months")
           .set("date", day),
         moment(this.myday)
-          .add(2, "months")
+          .add(2 + offset, "months")
           .set("date", day)
       ];
       days.forEach(item => {
